23c3b6edcb0b80e753b3eaa4678a569653f3f890
joshua <iuttn123@bsidesoft.com>
2021-01-25 19:02:19 +0900

    navbar-js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-'use strict'
+'use strict';
+
+const navbar = document.querySelector("#navbar");
+console.log(navbar)
+const navbarHeight = navbar.getBoundingClientRect().height;
+
 
 document.addEventListener('scroll', () =>{
-  console.log(window.scrollY)
+  if(window.scrollY > navbarHeight){
+    navbar.classList.add('navbar--dark');
+  }else{
+    navbar.classList.remove('navbar--dark');
+  }
 })
