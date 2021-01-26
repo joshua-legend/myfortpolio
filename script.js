a1c052fcf83ba34ea959a6396014ef5021811bd6
joshua <iuttn123@bsidesoft.com>
2021-01-26 12:05:11 +0900

    arrow-up

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const navbar = document.querySelector("#navbar");
-console.log(navbar)
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 
+/*Make navbar*/
 document.addEventListener('scroll', () =>{
   if(window.scrollY > navbarHeight){
     navbar.classList.add('navbar--dark');
@@ -12,3 +12,44 @@ document.addEventListener('scroll', () =>{
     navbar.classList.remove('navbar--dark');
   }
 })
+
+
+/*Handle scrolling when tapping on the navbar menu*/
+
+const navbarMenu = document.querySelector(".navbar__menu")
+navbarMenu.addEventListener('click',(event)=>{
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) return;
+  const scrollTo = document.querySelector(link)
+  scrollTo.scrollIntoView({behavior:"smooth"})
+})
+
+/*Transparent when you scroll*/
+
+const home = document.querySelector('.home__container')
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+  home.style.opacity = 1 - window.scrollY/homeHeight;
+})
+
+/* Show "arrow up" button when scrooling down */
+
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', ()=>{
+  if(window.scrollY>homeHeight/2){
+    arrowUp.classList.add('visible');
+  }else{
+    arrowUp.classList.remove('visible');
+  }
+});
+
+arrowUp.addEventListener('click',()=>{
+  scrollIntoView('#home')
+})
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector)
+  scrollTo.scrollIntoView({behavior:'smooth'})
+}
