257b0631a89142ea4d50cf643d2e5eecc5306513
joshua <iuttn123@bsidesoft.com>
2021-01-28 16:21:53 +0900

    js-finish

--- script.js
+++ script.js
@@ -53,3 +53,38 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector)
   scrollTo.scrollIntoView({behavior:'smooth'})
 }
+
+/* Projects */
+
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work__projects')
+const projects = document.querySelectorAll('.project')
+
+workBtnContainer.addEventListener('click',(e)=>{
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode
+  e.target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(()=>{
+    projects.forEach((project)=>{
+      if(filter ==='*' || filter === project.dataset.type){
+        project.classList.remove('invisible')
+      }else{
+        project.classList.add('invisible')
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  },300)
+})
+
+
+/* Navbar toggle button for small screen */
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn')
+navbarToggleBtn.addEventListener('click',()=>{
+  navbarMenu.classList.toggle('open');
+})
+
